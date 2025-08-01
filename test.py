7fcbc5dbe8b8fa36ea3ef0ddb9fe85222c7195a2
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2025-08-01 12:43:10 -0400

    Beta Prototype

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-from systems.yes45tu_front_set import calculate_yes45tu_quantities
+from utils.formulas import calculate_door_info
 
-calculate_yes45tu_quantities(4,2,1,144,96,[{'size': "3' X 7'", 'count': 1, 'stile': 'Narrow', 'hardware': ['Continuous Hinges']}])
+v= calculate_door_info([{'size': "3' X 7'", 'count': 1, 'stile': 'Narrow', 'hardware': ['Continuous Hinges']}])
+print(v)
+
+door_items = calculate_door_info(doors) if doors else []
+        calculated_outputs.extend(door_items)
